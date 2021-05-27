58593a6b59ef910efcf4da25e5241c6c0be213f0
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-05-27 12:09:21 +0530

    added paths + minor fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layout/Navbar';
+import CreateProject from './components/projects/CreateProject';
 import ProjectDetails from './components/projects/ProjectDetails';
 
 function App() {
@@ -13,6 +15,8 @@ return(
         <Route exact path='/' component={Dashboard} />
         <Route path='/project/:id' component={ProjectDetails} />
         <Route path='/signin' component={SignIn} />
+        <Route path='/signup' component={SignUp} />
+        <Route path='/create' component={CreateProject} />
       </Switch>
     </div>
   </Router>
