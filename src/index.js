4d2e1591f5d8a3e4d87d20d92e53880067cf7b7c
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-05-29 17:50:40 +0530

    feat: added thunk middleware via store enhancers

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import rootReducer from './store/reducers/rootReducer';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
