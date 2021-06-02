80fac509a534b11fe5fcce76f8890eee38c3d6f0
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-06-02 12:13:09 +0530

    added RRF Provider to the app and rrfProps object

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { getFirestore, reduxFirestore } from 'redux-firestore';
-import { getFirebase, reduxFirebase } from 'react-redux-firebase';
+import { getFirebase, ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
 
 import './index.css';
 import App from './App';
 import rootReducer from './store/reducers/rootReducer';
-import fbConfig from './config/fbConfig';
+import firebase from './config/fbConfig';
 
 
 const store = createStore(rootReducer, 
-  compose(
-    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reduxFirestore(fbConfig),
-    reduxFirebase(fbConfig)
-  )
+    applyMiddleware(thunk.withExtraArgument({ getFirebase })),
 );
+
+const rrfProps = {
+  firebase,
+  config: {},
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}><App /></Provider>
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps} >
+      <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
