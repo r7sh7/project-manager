d88c63d104f883068139cc36440602aa8dbbcc98
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-06-02 12:15:06 +0530

    added firestore and firebase reducers

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,11 +1,17 @@
 import { combineReducers } from 'redux';
+import { firebaseReducer } from 'react-redux-firebase';
+import { firestoreReducer } from 'redux-firestore';
+
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
 
 const rootReducer = combineReducers(
     {
+        firestore: firestoreReducer,
+        firebase: firebaseReducer,
         auth: authReducer,
-        project: projectReducer
+        project: projectReducer,
+        
     }
 )
 
