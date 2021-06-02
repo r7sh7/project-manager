92ee23da1b5ad153ff019940fffa0a9edd927542
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-06-02 12:14:26 +0530

    added listener to connect to firestore collection

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect } from 'react-redux-firebase';
+
 import ProjectList from '../projects/ProjectList';
 import Notifications from './Notifications';
-import { useSelector } from 'react-redux';
+
 //dahsboard is our own class used to style as per our needs 
 //div(row) to use the materialize grid
 //div(col s12 m6) 12 columns with small screens and up and 6 columns for medium screens and up
 //offset-m1 is the gap between the two divs 1)Project List 2)Notifications
 const Dashboard = () => {
 
-    const projects = useSelector(state => state.project.projects);
-    console.log(projects);
+    useFirestoreConnect([
+        {collection:'projects'}
+    ])
+
+    const projects = useSelector(state => {
+        // console.log(state);
+        return (state.firestore.ordered.projects);
+    })
+    // const projects = useSelector(state => {
+    //     console.log(state);
+    //     return state.project.projects;
+    // });
 
     return (  
         <div className="dashboard container">
