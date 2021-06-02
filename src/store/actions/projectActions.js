f3e92489dae03516747d46a90cda2bf7261b94cd
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-06-02 12:15:50 +0530

    async code for creating a project in firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,19 @@
 export const createProjectAction = (project) => {
-    return(dispatch, getState, { getFirebase, getFirestore }) => {
+    return(dispatch, getState, { getFirebase }) => {
         //async call to fetch data
-        dispatch({type: 'CREATE_PROJECT', project});
+        const firestore = getFirebase().firestore();
+
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Levi',
+            authorLastName: 'Ackerman',
+            authorId: '1234',
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({type: 'CREATE_PROJECT', project});
+        }).catch((err) => {
+            dispatch({type: 'CREATE_PROJECT_ERROR', err})
+        })
+        
     }
 };
