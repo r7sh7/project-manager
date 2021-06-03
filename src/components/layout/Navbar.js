3a2a0171b6a12106df27d377ab637b82ecc2396c
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-06-03 13:06:34 +0530

    showing content based on auth status

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,18 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLink';
 import SignedOutLinks from './SignedOutLink';
+import { connect } from 'react-redux';
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+    // console.log(props);
+    const { auth} = props;
+    const link = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
     return (  //container is a materialize css class to contain the content inside the nav-wrapper which is a materialize css class
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">Project Manager</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                {link}
             </div>
         </nav>
     );
 }
- 
-export default Navbar;
+
+// const authStatus = useSelector(state => state.firebase.auth) can be used instead of mapStateToProps and connect 
+const mapStateToProps = (state) => {
+    return{
+        auth: state.firebase.auth
+    }
+}
+export default connect(mapStateToProps)(Navbar);
--- src/components/layout/SignedInLink.js
+++ src/components/layout/SignedInLink.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { NavLink } from "react-router-dom"; //we get an active class when the particular link is active
 
+import { signOut } from '../../store/actions/authActions'
+
     const SignedInLinks = () => {
+    
+    const dispatch = useDispatch();
+
+    const handleClick= () =>{
+        dispatch(signOut());
+    }
     return (  
           <ul className="right">
               <li><NavLink to='/create'>New Project</NavLink></li>
-              <li><NavLink to='/'>Log Out</NavLink></li>
+              <li><a onClick = {handleClick}> Log Out</a></li>
               <li><NavLink to='/' className="btn btn-floating pink lighten -1">HB</NavLink></li>
           </ul>
     );
