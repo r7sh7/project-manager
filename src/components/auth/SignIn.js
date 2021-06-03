2132ae03522a422fd9c0d21967dd38d1f3af3e06
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-06-03 12:42:51 +0530

    feat: handling user auth for Login and Logout

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react'
+import { useDispatch, connect } from 'react-redux'
 
-function SignIn() {
+
+import { signIn } from '../../store/actions/authActions'
+
+const SignIn = (props) => {
+
+    // const dispatch = useDispatch(); //instead of mapDispatchToProps and connect 
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const {authError, signIn} = props;
 
     const handleSubmit = (e) => {
 
             e.preventDefault();
-            console.log({email: email, password: password});
+            // console.log({email: email, password: password});
+            signIn({email: email, password: password})
+            // dispatch(signIn({email: email, password: password})) //if useDispatch was used 
     }
 
     return (
@@ -26,9 +35,25 @@ function SignIn() {
                 <div className="input-field">
                     <button className="btn pink lighten-1 z-depth-0">Log In</button>
                 </div>
+                <div className="red-text center">
+                    {authError? <p>{authError}</p> : null}
+                </div>
             </form>
         </div>
     );
 }
 
-export default SignIn;
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        signIn : (creds) => dispatch(signIn(creds))
+    }
+}
+
+const mapStateToProps = (state) => {
+    return{
+        authError: state.auth.authError //(state) of rootReducer -> authReducer(auth) -> property(initState) of authReducer (authError)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,10 +1,28 @@
 const initState = {
-
+    authError: null
 }
 
 const authReducer = (state = initState, action) => {
-
-    return state;
+    switch(action.type){
+        case 'LOGIN_SUCCESS':
+            console.log('Login success');
+            return{
+                ...state,
+                authError: null
+            }
+        case 'LOGIN_ERROR':
+            console.log('Login error')
+            return{
+                ...state,
+                authError: 'Login Failed'
+            }
+        case 'LOGOUT_SUCCESS':
+            console.log('logout success');
+            return state;
+            
+        default: return state;
+    }
+    
 }
 
 export default authReducer;
