39751fbe4b9bfd2b6d6e97052439bbbb5e42c079
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-06-06 17:25:17 +0530

    feat: auth and oncreate cloud functions for notifs

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
+admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,3 +9,37 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const createNotification = (notif => {
+    return admin.firestore().collection('notifications')
+                .add(notif).then(doc => console.log('notif added', doc));
+})
+
+
+exports.createdProject = functions.firestore.document('projects/{projectId}')
+                .onCreate(doc => {
+                    const project = doc.data();
+                    const notification = {
+                        content: 'New Project Added',
+                        user: `${project.authorFirstName} ${project.authorLastName}`,
+                        time: admin.firestore.FieldValue.serverTimestamp()
+                    }
+                
+                return createNotification(notification);
+                });
+
+exports.userSignup = functions.auth.user()
+            .onCreate(user => {
+                return admin.firestore().collection('users')
+                    .doc(user.uid).get().then((doc) => {
+                        const newUser = doc.data();
+                        const notification = {
+                            content: 'Joined the Party',
+                            user: `${newUser.firstName} ${newUser.lastName}`,
+                            time: admin.firestore.FieldValue.serverTimestamp()
+                        }
+
+                        return createNotification(notification);
+                    })
+                
+            })
